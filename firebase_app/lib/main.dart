4e50082fb70bd984158695d3fef5c4d11dc7dd5a
baiyuechuz <baiyuechuu@gmail.com>
2025-09-10 01:04:11 +0700

    feat: config to connect to firebase

--- firebase_app/lib/main.dart
+++ firebase_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'components/custom_button.dart';
 import 'pages/authentication.dart';
 import 'pages/database.dart';
@@ -10,6 +11,21 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await dotenv.load(fileName: ".env");
+
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: dotenv.env['FIREBASE_API_KEY']!,
+      authDomain: dotenv.env['FIREBASE_AUTH_DOMAIN']!,
+      projectId: dotenv.env['FIREBASE_PROJECT_ID']!,
+      storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET']!,
+      messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID']!,
+      appId: dotenv.env['FIREBASE_APP_ID']!,
+      databaseURL: dotenv.env['FIREBASE_DATABASE_URL'],
+      measurementId: dotenv.env['FIREBASE_MEASUREMENT_ID'],
+    ),
+  );
+
+  debugPrint('Firebase initialized successfully');
 
   runApp(const MyApp());
 }
